16baa6e533e28be39e4f45b4217024e019944199
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-07-05 21:15:48 +0700

    var1

--- src/main/kotlin/com/prilepskiy/treningWeb1/controller/RestApiOfferController.kt
+++ src/main/kotlin/com/prilepskiy/treningWeb1/controller/RestApiOfferController.kt
@@ -6,16 +6,48 @@ import com.prilepskiy.treningWeb1.domain.interactors.GetMerchantListUseCase
 import com.prilepskiy.treningWeb1.domain.interactors.GetOfferListUseCase
 import com.prilepskiy.treningWeb1.domain.model.MerchantModel
 import com.prilepskiy.treningWeb1.domain.model.OfferModel
+import org.hibernate.internal.util.collections.CollectionHelper.listOf
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/offers")
-class RestApiOfferController(private val addOfferUseCase: AddOfferUseCase, private val getOfferListUseCase: GetOfferListUseCase) {
+class RestApiOfferController(private val addOfferUseCase: AddOfferUseCase, private val getOfferListUseCase: GetOfferListUseCase,getMerchantListUseCase: GetMerchantListUseCase) {
     init {
+        val temp:MutableList<MerchantModel> = listOf()
+        val temp2:MutableList<MerchantModel> = listOf()
+        val temp3:MutableList<MerchantModel> = listOf()
+        val temp4:MutableList<MerchantModel> = listOf()
+        getMerchantListUseCase().forEach {
 
-//Добавлю изначальную инициализацию
+
+            when(it.id){
+            in  1..3->{
+                temp.add(it)
+            }
+              in 11..13->{
+                  temp2.add(it)
+              }
+              in 21..30->{
+                  temp3.add(it)
+              }
+
+              in 31..40->{
+                  temp4.add(it)
+              }
+             else -> {
+
+             }
+
+          }
+
+        }
+        addOfferUseCase(OfferModel(1,"offer1","2.48$",temp))
+        addOfferUseCase(OfferModel(2,"offer2","3.48$",temp2))
+        addOfferUseCase(OfferModel(3,"offer3","4.48$",temp3))
+        addOfferUseCase(OfferModel(4,"offer4","5.48$",temp4))
+
     }
 
     @GetMapping
