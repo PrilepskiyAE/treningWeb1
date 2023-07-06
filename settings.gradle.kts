64144087151aa51ec4c4862343e82453de0e168f
Alexey Prilepskiy <alexey.prilepskiy@btb.dev>
2023-07-06 14:40:42 +0700

    Revert "Initial commit"
    This reverts commit 30b8eab6

--- settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "treningWeb1"
--- src/main/kotlin/com/prilepskiy/treningWeb1/domain/repository/MerchantRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.prilepskiy.treningWeb1.domain.repository
-
-import com.prilepskiy.treningWeb1.data.model.MerchantEntity
-import org.springframework.data.repository.CrudRepository
-
-interface MerchantRepository: CrudRepository<MerchantEntity,Int> {
-}
